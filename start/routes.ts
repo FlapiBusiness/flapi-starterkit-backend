31fd90e7468d72916613dc2e2efa4d1b8b969fea
Corentin Recanzone <corentindevpro@hotmail.com>
2024-12-20 11:53:25 +0100

    m

--- start/routes.ts
+++ start/routes.ts
@@ -1,17 +1,10 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
 import router from '@adonisjs/core/services/router'
-import AutoSwagger from 'adonis-autoswagger'
-import swagger from '#config/swagger'
-import { middleware } from '#start/kernel'
 
-const HealthController = () => import('#controllers/health_controller')
+/**
+ * Import des routes
+ */
+import './routes/swagger.ts'
+import './routes/health.ts'
 
 /**
  * Cette route est utilisée pour tester le fonctionnement de base de l'application.
@@ -20,25 +13,4 @@ router.get('/', async (): Promise<{ hello: string }> => {
   return {
     hello: 'world',
   }
-})
-
-/**
- * Vérifie l'état de santé de l'application
- */
-router.get('/health', [HealthController]).use(middleware.health())
-
-/**
- * Swagger YAML output
- */
-router.get('/swagger', async (): Promise<any> => {
-  return AutoSwagger.default.docs(router.toJSON(), swagger)
-})
-
-/**
- * Swagger UI
- */
-router.get('/docs', async (): Promise<string> => {
-  return AutoSwagger.default.ui('/swagger', swagger)
-  // return AutoSwagger.default.scalar("/swagger")
-  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
 })
